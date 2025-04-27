4cb6aa16f5436084681621251ce029c2ff2e4c2a
Neuxs0 <Neuxsyt@gmail.com>
2025-04-27 02:53:43 -0700

    Position button in OptionsMenu to the Sound Slider

--- src/common/main/java/dev/neuxs/sound_controller/Mod.java
+++ src/common/main/java/dev/neuxs/sound_controller/Mod.java
@@ -1,12 +1,17 @@
 package dev.neuxs.sound_controller;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import dev.neuxs.sound_controller.settings.SettingsManager;
-import dev.neuxs.sound_controller.utils.SoundHelper;
+import finalforeach.cosmicreach.ui.widgets.CRButton;
+import finalforeach.cosmicreach.ui.widgets.CRSlider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mod {
     public static final Logger LOGGER = LoggerFactory.getLogger("Sound Controller");
+    public static CRSlider capturedSoundSlider = null;
+    public static Table capturedTable = null;
+    public static CRButton soundControllerButton = null;
 
     public static void init() {
         SettingsManager.initialize();
--- src/common/main/java/dev/neuxs/sound_controller/mixins/OptionsMenuMixin.java
+++ src/common/main/java/dev/neuxs/sound_controller/mixins/OptionsMenuMixin.java
@@ -1,36 +1,79 @@
 package dev.neuxs.sound_controller.mixins;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import dev.neuxs.sound_controller.Mod;
 import dev.neuxs.sound_controller.ui.SoundControllerMenu;
 import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.gamestates.OptionsMenu;
-import finalforeach.cosmicreach.ui.FontRenderer;
 import finalforeach.cosmicreach.ui.widgets.CRButton;
+import finalforeach.cosmicreach.ui.widgets.CRSlider;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SuppressWarnings("unused")
 @Mixin(OptionsMenu.class)
 public abstract class OptionsMenuMixin extends GameState {
+    @Unique
+    private final Vector2 tmpVec = new Vector2();
 
-    @Inject(method = "create", at = @At("RETURN"))
-    private void sound_controller$addSoundToggleButtonToStage(CallbackInfo ci) {
-        if (this.stage == null) return;
+    @ModifyVariable(method = "create()V", at = @At(value = "STORE"), name = "soundSlider", require = 1)
+    private CRSlider captureSoundSlider(CRSlider originalSlider) {
+        Mod.capturedSoundSlider = originalSlider;
+        return originalSlider;
+    }
+
+    @ModifyVariable(method = "create()V", at = @At(value = "STORE"), name = "table", require = 1)
+    private Table captureTable(Table originalTable) {
+        Mod.capturedTable = originalTable;
+        return originalTable;
+    }
+
+    @Inject(method = "create()V", at = @At("RETURN"))
+    private void sound_controller$createSoundToggleButton(CallbackInfo ci) {
+        if (this.stage == null) {
+            return;
+        }
 
         final String buttonText = "Sound Controller";
-        final Vector2 buttonTextDim = FontRenderer.getTextDimensions(this.uiViewport, buttonText, new Vector2(0f,0f));
+        final float buttonWidth = 150f;
+        final float buttonHeight = 50f;
 
         CRButton soundToggleButton = new CRButton(buttonText) {
             @Override
             public void onClick() {
                 super.onClick();
-                GameState.switchToGameState(new SoundControllerMenu(GameState.currentGameState));
+                GameState.switchToGameState(new SoundControllerMenu(OptionsMenuMixin.this));
             }
         };
-        soundToggleButton.setSize(buttonTextDim.x + 10f, 50f);
-        soundToggleButton.setPosition(5f, 5f);
+
+        soundToggleButton.setSize(buttonWidth, buttonHeight);
+
+        Mod.soundControllerButton = soundToggleButton;
         this.stage.addActor(soundToggleButton);
+    }
+
+    @Inject(method = "render()V", at = @At("TAIL"))
+    private void sound_controller$positionSoundToggleButton(CallbackInfo ci) {
+        if (Mod.capturedSoundSlider != null &&
+                Mod.soundControllerButton != null &&
+                Mod.capturedSoundSlider.getStage() != null)
+        {
+            CRSlider slider = Mod.capturedSoundSlider;
+            CRButton button = Mod.soundControllerButton;
+
+            Vector2 sliderStageCoords = slider.localToStageCoordinates(tmpVec.set(0, 0));
+
+            if (slider.getWidth() > 0 || slider.getHeight() > 0) {
+                float buttonX = sliderStageCoords.x - button.getWidth() - 10f;
+                float buttonY = sliderStageCoords.y + (slider.getHeight() / 2f) - (button.getHeight() / 2f);
+
+                button.setPosition(buttonX, buttonY);
+            }
+        }
     }
 }
